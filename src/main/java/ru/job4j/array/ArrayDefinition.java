906eac7b56f82a95df1d874a248701943b2a8b9b
VladimirKozemaslov <kvs@intelins.ru>
2022-04-18 11:31:25 +0500

    6.0.2. Заполнение массива. [#226300]

--- src/main/java/ru/job4j/array/ArrayDefinition.java
+++ src/main/java/ru/job4j/array/ArrayDefinition.java
@@ -8,5 +8,16 @@ public class ArrayDefinition {
         System.out.printf("Длина массива surnames: %d\n", surnames.length);
         float[] prices = new float[40];
         System.out.printf("Длина массива prices: %d\n", prices.length);
+
+        String[] names = new String[4];
+        names[0] = "Александр Пушкин";
+        names[1] = "Лев Толстой";
+        names[2] = "Федор Достоевский";
+        names[3] = "Иван Гончаров";
+        System.out.println(names[0]);
+        System.out.println(names[1]);
+        System.out.println(names[2]);
+        System.out.println(names[3]);
+
     }
 }
